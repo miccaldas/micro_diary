475013ef8dfdffe53b49e00a857e37a9d2d40e20
micaldas <micaldas@mailfence.com>
2021-08-07 23:15:50 +0100

    Third Commit

--- post.py
+++ post.py
@@ -3,31 +3,31 @@ import urwid
 
 
 def exit_on_q(key):
-    if key in ('q', 'Q'):
+    if key in ("q", "Q"):
         raise urwid.ExitMainLoop()
 
 
 class QuestionBox(urwid.Filler):
     def keypress(self, size, key):
-        if key != 'enter':
+        if key != "enter":
             return super(QuestionBox, self).keypress(size, key)
         texto = u"%s.\n\nPress Q to exit." % edit.edit_text
-        self.original_widget = urwid.Text(texto, align='center')
+        self.original_widget = urwid.Text(texto, align="center")
 
 
 palette = [
-    ('banner', 'white', '#ff6f69'),
-    ('streak', 'white', 'light red'),
-    ('bg', 'white', '#ff6f69'),
+    ("banner", "white", "#ff6f69"),
+    ("streak", "white", "light red"),
+    ("bg", "white", "#ff6f69"),
 ]
 
-edit = urwid.Edit(('banner', u"   DEITA CÀ PRA FORA!  \n"), align='center')
+edit = urwid.Edit(("banner", u"   DEITA CÀ PRA FORA!  \n"), align="center")
 fill = QuestionBox(edit)
 loop = urwid.MainLoop(fill, palette, unhandled_input=exit_on_q)
 loop.screen.set_terminal_properties(colors=256)
-loop.widget = urwid.AttrMap(fill, 'bg')
+loop.widget = urwid.AttrMap(fill, "bg")
 loop.run()
 
-f = open('post.txt', 'w')
+f = open("post.txt", "w")
 f.write(edit.get_edit_text())
 f.close()
