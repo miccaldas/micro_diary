f215df83ec1f315bb8a57f013b686f804fac64ee
micaldas <micaldas@mailfence.com>
2021-09-29 18:11:14 +0100

    Update

--- connections.py
+++ connections.py
@@ -1,21 +1,31 @@
-"""Missing a comment here"""
+"""Module that inserts to database the values collected from the other files"""
 from mysql.connector import connect, Error
 
 
 def connections():
-    """E outro aqui"""
-    f = open('post.txt', 'r')
-    f_tags = open('tags.txt', 'r')
+    """
+    Opens two files and drops their content
+    into a database.
+
+    Args:
+        f: file, file with post text, open for reading
+        f_tags: file, file with the tags belonging to said post
+        conn: list, connects to the database
+        answers: list, collects the post text and the tags text in a list
+        query: str, "Insert into db (text, tags) VALUES (X, X)"
+        cur.execute: tuple, Executes the SQL query
+
+    Raise:
+        MySQLerror as e: Error while connecting to db
+    """
+    f = open("post.txt", "r")
+    f_tags = open("tags.txt", "r")
 
     post_content = f.read()
     tags_content = f_tags.read()
 
     try:
-        conn = connect(
-                host="localhost",
-                user="mic",
-                password="xxxx",
-                database="micro_diary")
+        conn = connect(host="localhost", user="mic", password="xxxx", database="micro_diary")
         cur = conn.cursor()
         answers = [post_content, tags_content]
         query = """ INSERT INTO micro_diary (text, tags) VALUES (%s, %s) """
@@ -24,12 +34,12 @@ def connections():
     except Error as e:
         print("Error while connecting to db", e)
     finally:
-        if(conn):
+        if conn:
             conn.close()
 
     f.close()
     f_tags.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     connections()
--- post.py
+++ post.py
@@ -1,14 +1,32 @@
-"""No comments on a subject that was new"""
+"""Defines the setup of where the posts will be written"""
 import urwid
 
 
 def exit_on_q(key):
+    """
+    Sets q/Q as the keys to exit Urwid
+    main loop.
+
+    Args:
+        key: str, Exits application main loop
+    """
     if key in ("q", "Q"):
         raise urwid.ExitMainLoop()
 
 
 class QuestionBox(urwid.Filler):
+    """Urwid class that defines a box with editable content"""
+
     def keypress(self, size, key):
+        """
+        Defines what keypresses will be taken into account for
+        the application.
+
+        Args:
+            key: str, Return all keypresses, except enter, that
+            is used as 'activate'.
+            texto: str, Reminds user of the exit key.
+        """
         if key != "enter":
             return super(QuestionBox, self).keypress(size, key)
         texto = u"%s.\n\nPress Q to exit." % edit.edit_text
